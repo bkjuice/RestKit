b07a33f563c4923624de232d6a2f7281a61d475b
bruce krakower <bruce.krakower@gmail.com>
2016-10-24 10:04:48 -0400

    Version revision to update/correct nuget.org metadata.

--- src/RestKit/Properties/AssemblyInfo.cs
+++ src/RestKit/Properties/AssemblyInfo.cs
@@ -12,6 +12,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("de3a6223-af0a-4583-bdfb-63957829e7ba")]
-[assembly: AssemblyVersion("1.1.0.1")]
-[assembly: AssemblyFileVersion("1.1.0.1")]
+[assembly: AssemblyVersion("1.1.0.2")]
+[assembly: AssemblyFileVersion("1.1.0.2")]
 [assembly: CLSCompliant(true)]
